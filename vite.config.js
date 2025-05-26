5dd2b9b58161668c3c44be389014d062e1632782
Simon Achkar <simon.achkar@ibm.com>
2025-05-26 14:18:34 -0300

    Removed comment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // CRA's default build output
+    outDir: "build",
   },
   test: {
     globals: true,
